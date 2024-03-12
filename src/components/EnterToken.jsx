26da7130175aa08ae5e5d0eb0f10198bb18871ca
NoisyBotDude <ashishsarmah11@gmail.com>
2024-03-12 21:45:57 +0530

    Add EnterToken and HaveTokenTabs components

---
+++ src/components/EnterToken.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function EnterToken(props) {
+    return (
+        <>
+            <div className="formbold-form-step-2-authenticate" id="formbold-steps-tab-authenticate">
+                    <div>
+                        <label for="token" className="formbold-form-label"> Enter your Authentication Token:
+                        </label>
+                        <textarea rows="6" name="token" id="message" placeholder="Enter your Token ..."
+                            className="formbold-form-input" value={props.token}></textarea>
+                    </div>
+                    <div>
+                        <button className="btn btn-primary mt-2 mb-3" >Next</button>
+                    </div>
+                </div>
+        </>
+    )
+}
